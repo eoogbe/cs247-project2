e6b24bf4850b28cab9b6ccdd52ef832a0e0cf653
Evawere Ogbe <eoogbe@stanford.edu>
2015-01-21 01:49:07 -0800

    added animations

--- main.js
+++ main.js
@@ -3,11 +3,41 @@ $(function(){
         var viewHeight = $(window).height();
         $(".page").css("height", viewHeight);
     }
-    $(window).resize(setPageSize);
-    setPageSize();
+    
+    function highlightActiveNavLink(top) {
+        $(".nav-boxes > li").removeClass("active-box");
+        var pageNo = Math.floor(top / $(window).height());
+        if (pageNo > 2) pageNo = 2;
+        if (pageNo > 0) {
+            $(".nav-boxes > li:nth-child(" + pageNo + ")").addClass("active-box");
+        }
+    }
+    
+    function smoothScroll(top) {
+        $("html, body").animate({ scrollTop: top }, {
+            duration: 1000,
+            complete: function() {
+                highlightActiveNavLink(top);
+            }
+        });
+    }
+    
+    $(window).scroll(function() {
+        highlightActiveNavLink($("body").scrollTop());
+    });
+    
+    $(".nav-boxes > li > a").click(function(e){
+        e.preventDefault();
+        
+        var $target = $(this.hash);
+        smoothScroll($target.offset().top);
+    });
     
     $("#next-btn").click(function(){
-        var newTop = $("body").scrollTop() + $(window).height();
-        $("body").scrollTop(newTop);
+        var newTop = $("body").scrollTop() + $(window).height() + 18;
+        smoothScroll(newTop);
     });
+    
+    $(window).resize(setPageSize);
+    setPageSize();
 });
